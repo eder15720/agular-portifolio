17c7a700649cb132d0ccdfe74576ec66b20f75ac
eder15720 <eder15720@hotmail.com>
2024-04-22 19:28:06 -0300

    ajustando secao de projetos para forma responsiva

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -24,6 +24,34 @@ export class ProjectsComponent {
           href: 'https://vidafullstack.com.br',
         },
       ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: 'Vida FullStack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Atuação com PL/SQL em projetos para coleta de dados de estatistico e criação de mecanismo automatizado para inclusão do nono dígito em todas as linhas da operadora VIVO.',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: 'Vida FullStack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Atuação com PL/SQL em projetos para coleta de dados de estatistico e criação de mecanismo automatizado para inclusão do nono dígito em todas as linhas da operadora VIVO.',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ]
     }
   ]);
 
